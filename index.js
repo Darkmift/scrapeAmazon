57a5fac7f926a52226c634fee1df9bde4a1d7644
dark_mift <darkmift@gmail.com>
2019-03-11 16:05:39 +0200

    added optimization and remove redundant code

--- index.js
+++ index.js
@@ -1,60 +1,71 @@
-const puppeteer = require('puppeteer');
-const base_url = 'https://www.amazon.com/gp/bestsellers/';
+const puppeteer = require("puppeteer");
+const base_url = "https://www.amazon.com/gp/bestsellers/";
 
-puppeteer.launch({
-	headless: true,
-	args: ['--no-sandbox', '--disable-setuid-sandbox']
-}).then(
-	async browser => {
+puppeteer
+	.launch({
+		headless: true,
+		args: ["--no-sandbox", "--disable-setuid-sandbox"]
+	})
+	.then(async browser => {
 		const page = await browser.newPage();
 		await page.setViewport({
 			width: 320,
 			height: 600
 		});
 
-		await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
-		await page.goto(base_url, {
-			waitUntil: 'networkidle0'
+		await page.setUserAgent(
+			"Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1"
+		);
+
+		await page.setRequestInterception(true);
+		//abort image crawl
+		page.on('request', (req) => {
+			if (req.resourceType() === 'image') {
+				req.abort();
+			} else {
+				req.continue();
+			}
 		});
 
-		await page.waitForSelector('div#zg_col1');
-		await page.addScriptTag({
-			url: 'https://code.jquery.com/jquery-3.2.1.min.js'
-		})
+		await page.goto(base_url, {
+			waitUntil: "networkidle0"
+		});
 
-		console.log('Hi there!');
+		await page.waitForSelector("div#zg_col1");
+		await page.addScriptTag({
+			url: "https://code.jquery.com/jquery-3.2.1.min.js"
+		});
+
+		console.log("Hi there!");
 		//
 		const categories = await page.evaluate(() => {
 			try {
-				return Array.from(document.querySelectorAll('ul#zg_browseRoot li'))
-					.map(categories => categories.innerText.trim())
+				return Array.from(document.querySelectorAll("ul#zg_browseRoot li")).map(
+					categories => categories.innerText.trim()
+				);
 			} catch (err) {
 				reject(err.toString());
 			}
 		});
 		//
-		const title = await page.evaluate(() => {
+		const productInfo = await page.evaluate(() => {
 			try {
-				return Array.from(document.querySelectorAll('a.a-link-normal'))
-					.map(productUrl => productUrl.innerText.trim())
+				return Array.from(document.querySelectorAll("a.a-link-normal")).map(
+					product => ({
+						title: product.pathname.trim(),
+						url: product.pathname.trim()
+					})
+				);
 			} catch (err) {
 				reject(err.toString());
 			}
 		});
-		//
-		const productUrl = await page.evaluate(() => {
-			try {
-				return Array.from(document.querySelectorAll('a.a-link-normal'))
-					.map(productUrl => productUrl.pathname.trim())
-			} catch (err) {
-				reject(err.toString());
-			}
-		});
-		//
+
 		const productImages = await page.evaluate(() => {
 			try {
-				return Array.from(document.querySelectorAll('.a-dynamic-image.p13n-sc-dynamic-image'))
-					.map(productImages => productImages.src)
+				return Array.from(
+					document.querySelectorAll(".a-dynamic-image.p13n-sc-dynamic-image")
+				).map(productImages => productImages.src);
 			} catch (err) {
 				reject(err.toString());
 			}
@@ -62,9 +73,7 @@ puppeteer.launch({
 
 		console.log({
 			categories: categories,
-			title: title,
-			productUrl: productUrl,
-			productImages: productImages
+			productImages: productImages,
+			productInfo: productInfo
 		});
-
 	});
